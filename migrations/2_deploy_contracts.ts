046c39dd6d3e29d56431e572a27771584db2a1b2
mtw-programmer <visnia58@gmail.com>
2023-11-22 12:03:07 +0100

    refactor: include Users contract in deploy

--- migrations/2_deploy_contracts.ts
+++ migrations/2_deploy_contracts.ts
@@ -1,7 +1,8 @@
-import { Deployer } from 'truffle';
-
+const Users = artifacts.require('Users');
 const Transfer = artifacts.require('Transfer');
 
-export default (deployer: Deployer) => {
-  deployer.deploy(Transfer);
+export default async (deployer: { deploy: (contract: any, options?: any) => void }) => {
+  deployer.deploy(Users);
+  const UsersInstance = await Users.deployed();
+  deployer.deploy(Transfer, UsersInstance.address);
 };
