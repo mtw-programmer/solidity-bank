eaffb31f63497e3f7507b85faa1eb147bf598118
mtw-programmer <visnia58@gmail.com>
2023-11-22 09:45:29 +0100

    feat: create getUserId function and add accountQuantity

--- contracts/Transfer.sol
+++ contracts/Transfer.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 contract Transfer {
   address private owner;
 
+  uint private accountQuantity = 0;
   mapping(address => uint) private id;
   mapping(uint => uint256) private balance;
 
@@ -11,9 +12,18 @@ contract Transfer {
     owner = msg.sender;
   }
 
+  function getUserId(address _address) private returns (uint) {
+    if (id[_address] == 0) {
+      accountQuantity += 1;
+      id[_address] = accountQuantity;
+    }
+
+    return id[_address];
+  }
+
   event ToppedUp(uint account, uint256 amount, uint256 time);
 
   receive() external payable {
-    emit ToppedUp(id[msg.sender], msg.value, block.timestamp);
+    emit ToppedUp(getUserId(msg.sender), msg.value, block.timestamp);
   }
 }
