19bdd8cb2a54a8437badef123fc9e40059ef6495
mtw-programmer <visnia58@gmail.com>
2023-11-23 22:39:26 +0100

    feat: call Users addModifier and addFunds in Transfer

--- contracts/Transfer.sol
+++ contracts/Transfer.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.19;
 import './Users.sol';
 
 contract Transfer {
-  address private owner;
   Users private usersContract;
 
   constructor(address usersAddress) {
-    owner = msg.sender;
     usersContract = Users(usersAddress);
+    usersContract.addModifier(address(this));
   }
 
   event ToppedUp(uint account, uint256 amount, uint256 time);
 
   receive() external payable {
+    usersContract.addFunds(msg.sender, msg.value);
     emit ToppedUp(usersContract.getUserId(msg.sender), msg.value, block.timestamp);
   }
 }
