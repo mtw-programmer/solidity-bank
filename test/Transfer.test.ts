92c924a46822f5b19f8f9732b07b1f8bc2ce4a70
mtw-programmer <visnia58@gmail.com>
2023-11-26 19:24:42 +0100

    feat: add tests for transfer function

--- test/Transfer.test.ts
+++ test/Transfer.test.ts
@@ -5,10 +5,12 @@ import config from '../utils/config';
 const web3 = new Web3(`${config.PROTOCOL}://${config.HOST}:${config.PORT}`);
 
 const Transfer = artifacts.require('./Transfer.sol');
+const Users = artifacts.require('./Users.sol');
 
 describe('Transfer Contract:', () => {
   before(async function () {
     this.transfer = await Transfer.deployed();
+    this.users = await Users.deployed();
     this.accounts = await web3.eth.getAccounts();
   });
 
@@ -34,6 +36,62 @@ describe('Transfer Contract:', () => {
       assert.exists(toppedUpEvent, 'ToppedUp event should be emitted');
       assert.notEqual(toppedUpEvent.args.account.toNumber(), 0, 'Invalid account id');
       assert.equal(toppedUpEvent.args.amount.toString(), web3.utils.toWei('1', 'ether'), 'Amount should match');
+    });
+  });
+
+  describe('[transfer]', function () {
+    it('fails when invalid address given', async function () {
+      await this.users.addFunds(this.accounts[4], 10, { from: this.accounts[0] });
+      try {
+        await this.transfer.transfer('a', 1, { from: this.accounts[0] });
+        assert.fail('Expected an error but did not get one');
+      } catch (ex:any) {
+        assert.exists(ex.message);
+      }
+    });
+
+    it('fails when 0x0 address given', async function () {
+      try {
+        await this.transfer.transfer(0x0, 1, { from: this.accounts[0] });
+        assert.fail('Expected an error but did not get one');
+      } catch (ex:any) {
+        assert.exists(ex.message);
+      }
+    });
+
+    it('fails when transfer to the same address', async function () {
+      try {
+        await this.transfer.transfer(this.accounts[4], 1, { from: this.accounts[4] });
+        assert.fail('Expected an error but did not get one');
+      } catch (ex:any) {
+        assert.include(ex.message, "You cannot transfer funds to your own account");
+      }
+    });
+
+    it('fails when invalid amount given', async function () {
+      try {
+        await this.transfer.transfer(this.accounts[1], 0, { from: this.accounts[4] });
+        assert.fail('Expected an error but did not get one');
+      } catch (ex:any) {
+        assert.exists(ex.message);
+      }
+    });
+
+    it('fails when amount is not on the account', async function () {
+      try {
+        await this.transfer.transfer(this.accounts[0], 10, { from: this.accounts[1] });
+        assert.fail('Expected an error but did not get one');
+      } catch (ex:any) {
+        assert.include(ex.message, "Insufficient amount on the account");
+      }
+    });
+
+    it('successfully transfer balances', async function () {
+      await this.transfer.transfer(this.accounts[5], 1, { from: this.accounts[4] });
+      const balanceReceiver = await this.users.getUserBalance.call(this.accounts[5], { from: this.accounts[0] });
+      assert.equal(balanceReceiver.toNumber(), 1);
+      const balanceSender = await this.users.getUserBalance.call(this.accounts[4], { from: this.accounts[0] });
+      assert.equal(balanceSender.toNumber(), 9);
     });
   });
 });
