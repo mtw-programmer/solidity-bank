af1ddc6d23feb21e594891c7dceb65ecb4205e7e
mtw-programmer <visnia58@gmail.com>
2023-11-27 19:37:47 +0100

    feat: add ETC to migrations and modifiers

--- migrations/2_deploy_contracts.ts
+++ migrations/2_deploy_contracts.ts
@@ -2,11 +2,15 @@ import { Deployer } from 'truffle';
 
 const Users = artifacts.require('Users');
 const Transfer = artifacts.require('Transfer');
+const ETC = artifacts.require('ETC');
 
 export default async (deployer: Deployer) => {
   await deployer.deploy(Users);
   const UsersInstance = await Users.deployed();
   await deployer.deploy(Transfer, UsersInstance.address);
+  await deployer.deploy(ETC);
   const TransferInstance = await Transfer.deployed();
+  const ETCInstance = await ETC.deployed();
   await UsersInstance.addModifier(TransferInstance.address);
+  await UsersInstance.addModifier(ETCInstance.address);
 };
