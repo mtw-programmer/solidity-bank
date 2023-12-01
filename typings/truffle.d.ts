93bb4e451c1c1e7760417611c4966438dadb082e
mtw-programmer <visnia58@gmail.com>
2023-12-01 18:17:14 +0100

    fix: update deployer typing

--- typings/truffle.d.ts
+++ typings/truffle.d.ts
@@ -1,5 +1,5 @@
 declare module 'truffle' {
   export interface Deployer {
-    deploy: (contract: any, options?: any) => void;
+    deploy: (contract: any, contractA?: any, contractB?: any) => void;
   }
 }
