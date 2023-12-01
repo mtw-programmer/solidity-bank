d476428336df4e42b4ecc9b58430145a9e79d71d
mtw-programmer <visnia58@gmail.com>
2023-12-01 18:16:34 +0100

    feat: include Users contract import in deployer

--- migrations/2_deploy_contracts.ts
+++ migrations/2_deploy_contracts.ts
@@ -8,8 +8,8 @@ export default async (deployer: Deployer) => {
   await deployer.deploy(Users);
   const UsersInstance = await Users.deployed();
   await deployer.deploy(Transfer, UsersInstance.address);
-  await deployer.deploy(ETC);
   const TransferInstance = await Transfer.deployed();
+  await deployer.deploy(ETC, UsersInstance.address, TransferInstance.address);
   const ETCInstance = await ETC.deployed();
   await UsersInstance.addModifier(TransferInstance.address);
   await UsersInstance.addModifier(ETCInstance.address);
