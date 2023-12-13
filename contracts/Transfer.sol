9e4c435f1b913b8b4d5820d44d16d61c1c210e86
mtw-programmer <visnia58@gmail.com>
2023-12-13 17:03:38 +0100

    style: update Transfer formatting

--- contracts/Transfer.sol
+++ contracts/Transfer.sol
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import './Users.sol';
+import "./Users.sol";
 
 contract Transfer {
-  Users private usersContract;
+    Users private usersContract;
 
-  constructor(address usersAddress) {
-    require(usersAddress != address(0), "Invalid Users contract address");
-    usersContract = Users(usersAddress);
-  }
+    constructor(address usersAddress) {
+        require(usersAddress != address(0), "Invalid Users contract address");
+        usersContract = Users(usersAddress);
+    }
 
-  event ToppedUp(uint account, uint256 amount, uint256 time);
+    event ToppedUp(address account, uint256 amount, uint256 time);
 
-  receive() external payable {
-    usersContract.addFunds(msg.sender, msg.value);
-    emit ToppedUp(usersContract.getUserId(msg.sender), msg.value, block.timestamp);
-  }
+    receive() external payable {
+        usersContract.addFunds(msg.sender, msg.value);
+        emit ToppedUp(msg.sender, msg.value, block.timestamp);
+    }
 
-  function transfer(address _to, uint256 _amount) external {
-    require(_to != msg.sender, "You cannot transfer funds to your own account");
-    usersContract.takeFunds(msg.sender, _amount);
-    usersContract.addFunds(_to, _amount);
-  }
+    function transfer(address _to, uint256 _amount) external {
+        require(
+            _to != msg.sender,
+            "You cannot transfer funds to your own account"
+        );
+        usersContract.takeFunds(msg.sender, _amount);
+        usersContract.addFunds(_to, _amount);
+    }
 }
